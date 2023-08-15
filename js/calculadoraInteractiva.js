8c2dc51c0200f6455e2bfa67635024fde4a7d726
lianivansinchez <maquiavelic1030@gmail.com>
2023-08-15 18:06:27 -0300

    Frase motivadora con bucle añadida a la calculadora

--- js/calculadoraInteractiva.js
+++ js/calculadoraInteractiva.js
@@ -1,24 +1,27 @@
-// Función para sumar
-function sumNum(a, b) {
+// función para sumar dos números
+function sumar(a, b) {
     return a + b;
 }
 
-// Función para restar
-function restarNum(a, b) {
+// función para restar dos números
+function restar(a, b) {
     return a - b;
 }
 
-// Función para dividir
-function divNum(a, b) {
-    return a / b;
-}
-
-// Función para multiplicar
-function multiNum(a, b) {
+// función para multiplicar dos números
+function multiplicar(a, b) {
     return a * b;
 }
 
-// Función principal de la calculadora
+// función para dividir dos números
+function dividir(a, b) {
+    if (b !== 0) {
+        return a / b;
+    } else {
+        return "Error: No se puede dividir por cero.";
+    }
+}
+
 function calculator() {
     const operation = prompt("Seleccione una operación: +, -, *, / (o 'salir' para salir)");
 
@@ -47,11 +50,27 @@ function calculator() {
             break;
         default:
             console.log('Operación inválida');
-            return; // Sale de la función si la operación es inválida
+            return;
     }
 
     alert(`Resultado: ${result}`);
 }
 
-// Iniciar la calculadora
-calculator();
+// función para imprimir números del 0 al 5
+function imprimirNumeros() {
+    for (let i = 1; i <= 5; i++) {
+        alert(i);
+    }
+
+    // Frase motivadora que se imprime después del bucle
+    alert("¡Persistencia y esfuerzo son la clave del éxito! ¡Sigue adelante!");
+}
+
+const userChoice = prompt('¿Desea usar la "Calculadora" o la "Impresora Motivadora" para imprimir numeros del 0 al 5 y recibir una frase motivadora? (Escribir "calculadora" o "impresora")')
+
+if(userChoice === 'calculadora'){
+    calculator();
+}
+else if(userChoice === 'impresora'){
+    imprimirNumeros();
+}
